d15865545026169ade243cdb69669adf0d6299b1
luischaparoc <lfchaparro@outlook.com>
2019-09-10 20:30:40 +0000

    [Fix]: concatenates each index Modified: 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def remove_char_at(str, n):
-    ln = len(str)
-    if n >= 0 and n <= ln:
-        l = list(str)
-        l[n] = ""
-        str = "".join(l)
-    return (str)
+    s = ""
+    for i in range(len(str)):
+        if i != n:
+            s = s + str[i]
+    return (s)
