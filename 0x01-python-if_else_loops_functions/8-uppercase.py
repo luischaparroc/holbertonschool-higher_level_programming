4c7cc459b9ea119bd3cc0dc162a4e3d1b8c279b0
luischaparoc <lfchaparro@outlook.com>
2019-09-10 18:06:58 +0000

    [Feature]: using ord Modified: 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if str[i] >= 'a' and str[i] <= 'z':
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             num = 32
         else:
             num = 0
