718a2636f24a0c0f586bea0e582792046721762a
luischaparoc <lfchaparro@outlook.com>
2019-09-10 15:09:32 +0000

    [Fix]: using format in 2 print calls Modified: 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for num in range(0, 100):
         if num != 99:
                 print("{:02d}, ".format(num), end='')
         else:
-                print(num)
+                print("{:02d}".format(num))
