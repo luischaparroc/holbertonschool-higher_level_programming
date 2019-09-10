e80067e8d4f6a423d674f36d904d7361c12d45f7
luischaparoc <lfchaparro@outlook.com>
2019-09-10 18:04:56 +0000

    [Feature]: adding a function that checks for lowercase chars Modified: 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c >= 'a' and c <= 'z':
-        return (1)
+    if ord(c) >= 97 and ord(c) <= 122:
+        return (True)
     else:
-        return (0)
+        return (False)
