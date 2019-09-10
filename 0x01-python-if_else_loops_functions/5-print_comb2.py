cfffa22be7d9f6ad57d1b16bf2ccbe20292fabb7
luischaparoc <lfchaparro@outlook.com>
2019-09-10 15:07:17 +0000

    [Fix]: set function to print 2 digits Modified: 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
         if num != 99:
-                print("{:d}, ".format(num), end='')
+                print("{:02d}, ".format(num), end='')
         else:
                 print(num)
