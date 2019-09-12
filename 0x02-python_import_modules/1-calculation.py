d9cc0fd9e75177dfd7bfc9f23292dd10b769ed26
luischaparoc <lfchaparro@outlook.com>
2019-09-12 13:39:20 +0000

    [Feature]: adding a program that imports functions from a file Created: 1-calculation.py

---
+++ 0x02-python_import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
