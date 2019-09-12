f377a1624de3c4313d073595263981c84503a584
luischaparoc <lfchaparro@outlook.com>
2019-09-12 18:05:41 +0000

    [Feature]: adding a Python function that does exactly the same as a given bytecode Modified: 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for i in range(90):
+        for i in range(4, 6):
             c = add(c, i)
         return (c)
     else:
