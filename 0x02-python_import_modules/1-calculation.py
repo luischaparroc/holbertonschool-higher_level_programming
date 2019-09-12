e13b4d0524bb1aa1c4032a27f05457f5b49fb440
luischaparoc <lfchaparro@outlook.com>
2019-09-12 14:38:08 +0000

    [Fix]: code is not executed when is imported Modified: 1-calculation.py

--- 0x02-python_import_modules/1-calculation.py
+++ 0x02-python_import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
