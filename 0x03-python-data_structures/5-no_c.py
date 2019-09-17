8b1735f20e971b870923be7c62f5a43a59ec2623
luischaparoc <lfchaparro@outlook.com>
2019-09-17 15:06:24 +0000

    [Feature]: adding a function that removes all chars C and C from a str Created: 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    length = len(my_string)
+
+    j = 0
+
+    new_string = my_string[:]
+
+    for i in range(length):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            new_string = new_string[:(i - j)] + my_string[(i + 1):]
+            j += 1
+
+    my_string = new_string
+
+    return (my_string)
