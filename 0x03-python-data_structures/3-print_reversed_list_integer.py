18a16631238793ef8fce56889ac17a2517202588
luischaparoc <lfchaparro@outlook.com>
2019-09-17 17:54:58 +0000

    [Fix]: it doesn't determines the length when list is empty Modified: 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
 
-    if length == 0:
+    if my_list == []:
         pass
     else:
+        length = len(my_list)
         for i in reversed(range(length)):
             print("{:d}".format(my_list[i]))
