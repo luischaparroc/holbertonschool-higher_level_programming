c1d3715454ef9e4d28f9425324a0860fdcd204d8
luischaparoc <lfchaparro@outlook.com>
2019-09-17 17:34:06 +0000

    [Fix]: prints a space where there is no integers Modified: 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
 
+    if length == 0:
+        print()
+
     for i in reversed(range(length)):
         print("{:d}".format(my_list[i]))
