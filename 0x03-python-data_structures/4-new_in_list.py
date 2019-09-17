979f55dc075e59613aab583d3ea70f257aca06d8
luischaparoc <lfchaparro@outlook.com>
2019-09-17 14:44:30 +0000

    [Fix]: function replaces the last element Modified: 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
 
     new_list = my_list[:]
 
-    if 0 <= idx < length - 1:
+    if 0 <= idx < length:
         new_list[idx] = element
 
     return (new_list)
