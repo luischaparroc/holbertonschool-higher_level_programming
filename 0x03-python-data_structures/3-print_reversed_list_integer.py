8c725439945033f2490b8338c0b795f847eaacbf
luischaparoc <lfchaparro@outlook.com>
2019-09-17 17:50:44 +0000

    [Fix]: doesn't go to the function when list is empty Modified: 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,6 @@ def print_reversed_list_integer(my_list=[]):
 
     if length == 0:
         pass
-
-    for i in reversed(range(length)):
-        print("{:d}".format(my_list[i]))
+    else:
+        for i in reversed(range(length)):
+            print("{:d}".format(my_list[i]))
