da3fbf1abfb76affec645798dc0e5f0da9bda4c7
luischaparoc <lfchaparro@outlook.com>
2019-09-17 17:45:08 +0000

    [Fix]: it doesn't print when list is empty Modified: 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
 
-    if my_list == []:
-        pass
-
-    for i in reversed(range(length)):
-        print("{:d}".format(my_list[i]))
+    if length != 0:
+        for i in reversed(range(length)):
+            print("{:d}".format(my_list[i]))
