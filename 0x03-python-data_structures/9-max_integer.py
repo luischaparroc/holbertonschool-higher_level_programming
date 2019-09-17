5ebe847db25b54ec6a03be8571bb2210c77217e4
luischaparoc <lfchaparro@outlook.com>
2019-09-17 16:04:36 +0000

    [Update]: reducing iterations in the loop Modified: 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,7 +7,7 @@ def max_integer(my_list=[]):
 
     max_int = my_list[0]
 
-    for i in range(length):
+    for i in range(1, length):
         if my_list[i] > max_int:
             max_int = my_list[i]
 
