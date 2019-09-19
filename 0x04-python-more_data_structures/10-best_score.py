8a183611e3bd3d6df7ed228db13a2f5e434243fc
luischaparoc <lfchaparro@outlook.com>
2019-09-19 18:24:33 +0000

    [Fix]: returns the key Modified: 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,10 +4,12 @@ def best_score(a_dictionary):
         return (None)
 
     list_keys = list(a_dictionary.keys())
+    name = list_keys[0]
     score = a_dictionary[list_keys[0]]
 
     for i in list_keys:
         if a_dictionary.get(i) > score:
+            name = i
             score = a_dictionary.get(i)
 
-    return (score)
+    return (name)
