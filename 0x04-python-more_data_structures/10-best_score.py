c5195830cb105c0de2a5e50bd69fea3542a24c47
luischaparoc <lfchaparro@outlook.com>
2019-09-19 19:25:40 +0000

    [Fix]: doesn't return None Modified: 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return (None)
-
-    return (max(a_dictionary, key=a_dictionary.get))
+    if a_dictionary is not None:
+        return (max(a_dictionary, key=a_dictionary.get))
