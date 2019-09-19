bb55dbd6a8c773423e2ab94975594f85020bcc3a
luischaparoc <lfchaparro@outlook.com>
2019-09-19 19:42:31 +0000

    [Update]: reducing command lines Modified: 11-mutiply_list_map.py

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    new_list = list(map(lambda x: x * number, my_list))
-    return (new_list)
+    return(list(map(lambda x: x * number, my_list)))
