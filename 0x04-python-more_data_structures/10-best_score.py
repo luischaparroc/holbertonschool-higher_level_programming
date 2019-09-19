3cc4ad7e2d640417d47262ae27a9e4557f6e167a
luischaparoc <lfchaparro@outlook.com>
2019-09-19 19:28:48 +0000

    [Fix]: change condition Modified: 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        return (max(a_dictionary, key=a_dictionary.get))
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
