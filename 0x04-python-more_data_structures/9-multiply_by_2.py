f5f6eafedd0f2f930c3acca7b42b76e5062f4632
luischaparoc <lfchaparro@outlook.com>
2019-09-19 18:06:09 +0000

    [Fix]: multiplying all values Modified: 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-
-    if a_dictionary is None:
-        return
-
     new_dir = a_dictionary.copy()
     list_keys = list(new_dir.keys())
 
     for i in list_keys:
-        if new_dir.get(i) % 2 == 0:
-            new_dir[i] *= 2
+        new_dir[i] *= 2
 
     return (new_dir)
