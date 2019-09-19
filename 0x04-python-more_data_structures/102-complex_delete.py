ef4a510c73255d3a56bb16f058d35aab812d09cd
luischaparoc <lfchaparro@outlook.com>
2019-09-19 22:52:44 +0000

    [Fix]: if dictionary doesn't exist, return dictionary Modified: 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if not a_dictionary:
-        return
-
-    if not value:
-        return
-
     list_keys = list(a_dictionary.keys())
 
     for value_dic in list_keys:
