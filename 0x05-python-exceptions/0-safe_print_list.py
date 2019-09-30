a83cf2fbfc223da928ec00786601f8ef1d3417dd
luischaparoc <lfchaparro@outlook.com>
2019-09-30 16:27:02 +0000

    [Update]: function prints any type of variable Modified: 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if not my_list:
-        print()
-        return (0)
+    count = 0
 
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
+            print("{}".format(my_list[i]), end='')
         except:
-            i -= 1
             break
+        else:
+            count += 1
 
     print()
-    return (i + 1)
+    return (count)
