0d8d8707d5118a2213c90306e949bf186efd9437
luischaparoc <lfchaparro@outlook.com>
2019-09-30 18:57:31 +0000

    [Feature]: adding a function that prints an integer Created: 100-safe_print_integger_err.py Modified: 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,11 +3,8 @@ def safe_function(fct, *args):
     import sys
     try:
         result = fct(*args)
-    except ZeroDivisionError:
-        sys.stderr.write("Exception: division by zero\n")
-        result = None
-    except IndexError:
-        sys.stderr.write("Exception: list index out of range\n")
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
         result = None
 
     return (result)
