cabdbfe56b7f92f77e1a3854a66481a062218205
luischaparoc <lfchaparro@outlook.com>
2019-10-01 22:33:07 +0000

    [Fix]: conditionals ordered Modified: 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,7 +20,7 @@ class Square:
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
@@ -36,11 +36,11 @@ class Square:
         """
         if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(value[0], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(value[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -60,7 +60,7 @@ class Square:
         else:
             for i in range(self.position[1]):
                 print()
-            for i in range(self.size):
+            for i in range(0, self.size):
                 for k in range(self.position[0]):
                     print(" ", end='')
                 for j in range(self.size):
