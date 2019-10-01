648bf3b4ec3e69926367d37d7830d091fdc79acc
luischaparoc <lfchaparro@outlook.com>
2019-10-01 19:11:01 +0000

    [Feature]: adding a class that prints a # square Modified: 3-square.py Created: 5-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,7 +10,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = int(size)
+            self.__size = size
 
     def area(self):
         """ Method that returns the square are of the object
