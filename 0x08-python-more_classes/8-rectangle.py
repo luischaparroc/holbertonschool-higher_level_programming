1fda3b289d17035f0280767a1e46e09026f31535
luischaparoc <lfchaparro@outlook.com>
2019-10-08 00:28:16 +0000

    [Fix]: fixing TypeError messages Modified: 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -173,9 +173,9 @@ class Rectangle:
         """
 
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance")
+            raise TypeError("rect_2 must be an instance of Rectangle")
 
         if rect_1.area() >= rect_2.area():
             return rect_1
