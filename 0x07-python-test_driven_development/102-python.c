fb1848c9b9ed91eed469b5cd99481bc0979ba4e4
luischaparoc <lfchaparro@outlook.com>
2019-10-12 04:51:56 +0000

    [Feature]: adding function that prints string info Created: 102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
+
+/**
+ * print_python_string - Prints string information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
+void print_python_string(PyObject *p)
+{
+
+	char *string;
+	PyObject *str, *repr;
+
+	(void)repr;
+	printf("[.] string object info\n");
+
+	if (strcmp(p->ob_type->tp_name, "str"))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+
+	repr = PyObject_Repr(p);
+	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
+	string = PyBytes_AsString(str);
+	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
+	printf("  str = %s\n", string);
+}
