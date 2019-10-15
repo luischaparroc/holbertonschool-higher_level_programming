2c29f70ef9e111c25ad40297aa30e36b76db87e8
luischaparoc <lfchaparro@outlook.com>
2019-10-15 16:10:53 +0000

    [Feature]: adding a function that checks if obj type is a_class Created: 2-is_same_class.py

---
+++ 0x0A-python_inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """ Function that returns True/False if obj is a type of a_class
+
+    Args:
+        obj: object
+        a_class: class type
+
+    Returns:
+        True if type of obj is a_class
+        False, otherwise
+    """
+    return type(obj) is a_class
