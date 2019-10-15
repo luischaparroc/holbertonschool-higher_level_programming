f1e257149bbc2503e4a4809770227fe696a2bcf5
luischaparoc <lfchaparro@outlook.com>
2019-10-15 18:57:43 +0000

    [Feature]: adding a class Rectangle that inherits from BG class Created: 8-rectangle.py

---
+++ 0x0A-python_inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class that defines a rectangle from BaseGeometry Class """
+
+    def __init__(self, width, height):
+        """ Initializes instance """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
