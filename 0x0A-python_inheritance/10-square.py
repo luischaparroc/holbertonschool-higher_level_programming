4477fff298f4caee4a5747c6778b7af6a5fec995
luischaparoc <lfchaparro@outlook.com>
2019-10-15 19:52:31 +0000

    [Feature]: adding a class Square Created: 10-square.py

---
+++ 0x0A-python_inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class that defines a Square from Rectangle class """
+    def __init__(self, size):
+        """ Method that initializes a Square """
+        self.integer_validator("square", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """ Method that returns a string with the area """
+        return super().area()
