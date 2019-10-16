cb622714a3e28d3d2828f2b748a1b633a815688c
luischaparoc <lfchaparro@outlook.com>
2019-10-16 08:46:52 +0000

    [Feature]: adding a function that reads from a file Created: 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module that contains a function that reads from a file """
+
+
+def read_file(filename=""):
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'r') as f:
+        read_data = f.read()
+        print(read_data, end='')
+    f.closed
