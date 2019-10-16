2849b5c27eddb4db73027a5e64d78972df5b2b51
luischaparoc <lfchaparro@outlook.com>
2019-10-16 20:36:38 +0000

    [Update]: optimizing code Modified: 14-pascal_triangle.py

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -11,21 +11,19 @@ def pascal_triangle(n):
     """
 
     matrix = []
+    prev = []
 
     for i in range(n):
         res_list = []
-        if i == 0:
-            res_list = [1]
-        else:
-            p1 = -1
-            p2 = 0
-            for j in range(len(prev) + 1):
-                if p1 == -1 or p2 == len(prev):
-                    res_list = res_list + [1]
-                else:
-                    res_list = res_list + [prev[p1] + prev[p2]]
-                p1 += 1
-                p2 += 1
+        p1 = -1
+        p2 = 0
+        for j in range(len(prev) + 1):
+            if p1 == -1 or p2 == len(prev):
+                res_list += [1]
+            else:
+                res_list += [prev[p1] + prev[p2]]
+            p1 += 1
+            p2 += 1
         matrix.append(res_list)
         prev = res_list[:]
 
