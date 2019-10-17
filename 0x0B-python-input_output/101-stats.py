b2a576981dcefe68b078c0edee62c97980d16977
luischaparoc <lfchaparro@outlook.com>
2019-10-17 03:21:03 +0000

    [Update]: reducing command lines Modified: 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -44,16 +44,10 @@ if __name__ == "__main__":
             if nlines % 10 == 0 and nlines is not 0:
                 magic.print_info()
 
-            try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
-                pass
-
+            list_line = [x for x in line.split(" ") if x.strip()]
+            magic.add_status_code(list_line[-2])
+            magic.size += int(list_line[-1].strip("\n"))
             nlines += 1
     except KeyboardInterrupt:
         magic.print_info()
         raise
-    magic.print_info()
