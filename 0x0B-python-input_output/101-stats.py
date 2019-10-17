9759c609664b885e55339bed8bbb4e78c5a5dd9c
luischaparoc <lfchaparro@outlook.com>
2019-10-17 03:14:27 +0000

    [Fix]: checks if conversion is possible Modified: 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -43,10 +43,13 @@ if __name__ == "__main__":
             if nlines % 10 == 0 and nlines is not 0:
                 magic.print_info()
 
-            list_line = [x for x in line.split(" ") if x.strip()]
-            magic.add_status_code(list_line[-2])
+            try:
+                list_line = [x for x in line.split(" ") if x.strip()]
+                magic.add_status_code(list_line[-2])
 
-            magic.size += int(list_line[-1].strip("\n"))
+                magic.size += int(list_line[-1].strip("\n"))
+            except:
+                pass
 
             nlines += 1
     except KeyboardInterrupt:
