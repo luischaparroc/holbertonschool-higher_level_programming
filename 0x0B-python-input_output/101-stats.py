8116eff724bade98692a89d80cc11b78b6f28a7a
luischaparoc <lfchaparro@outlook.com>
2019-10-17 03:05:31 +0000

    [Fix]: module is executed when is main Modified: 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -32,22 +32,24 @@ class Magic:
             if self.dic[key] is not 0:
                 print("{}: {:d}".format(key, self.dic[key]))
 
-magic = Magic()
-magic.init_dic()
-nlines = 0
 
-try:
-    for line in sys.stdin:
-        if nlines % 10 == 0 and nlines is not 0:
-            magic.print_info()
+if __name__ == "__main__":
+    magic = Magic()
+    magic.init_dic()
+    nlines = 0
 
-        list_line = [x for x in line.split(" ") if x.strip()]
-        magic.add_status_code(list_line[-2])
+    try:
+        for line in sys.stdin:
+            if nlines % 10 == 0 and nlines is not 0:
+                magic.print_info()
 
-        magic.size += int(list_line[-1].strip("\n"))
+            list_line = [x for x in line.split(" ") if x.strip()]
+            magic.add_status_code(list_line[-2])
 
-        nlines += 1
-except KeyboardInterrupt:
+            magic.size += int(list_line[-1].strip("\n"))
+
+            nlines += 1
+    except KeyboardInterrupt:
+        magic.print_info()
+        raise
     magic.print_info()
-    raise
-magic.print_info()
