50c6717fc05892c3fffce64094342904afdd6d0d
luischaparoc <lfchaparro@outlook.com>
2019-10-18 20:50:51 +0000

    [Feature]: adding a class Base Created: models/__init__.py and models/base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that contains class Base """
+
+
+class Base:
+    """ Class Base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initializes instances """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
