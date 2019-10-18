8fe7b85fd5a50f8732d74634e59bade949e0a29c
luischaparoc <lfchaparro@outlook.com>
2019-10-18 22:07:43 +0000

    [Feature]: adding the class Rectangle Created: models/rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Module that contains class Rectangle,
+inheritance of class Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Class Rectangle """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initializes instances """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ x getter """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ y getter """
+        return self.y
+
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        self.__y = value
