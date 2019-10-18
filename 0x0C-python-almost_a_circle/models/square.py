3ed68d02aaafc96eb67b05bb1a9cb3a04e5241b4
luischaparoc <lfchaparro@outlook.com>
2019-10-18 23:56:31 +0000

    [Feature]: creating class Square from Rectangle Created: models/square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module that contains class Square,
+inheritance of class Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Class Rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializes instances """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ str special method """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
