b663bd049730e43f31070fd027867501e3a768a3
luischaparoc <lfchaparro@outlook.com>
2019-10-21 14:41:28 +0000

    [Update]: removing size attribute Modified: models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ Initializes instances """
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
@@ -20,3 +21,12 @@ class Square(Rectangle):
         str_wh = "{}/{}".format(self.width, self.height)
 
         return str_square + str_id + str_xy + str_wh
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
