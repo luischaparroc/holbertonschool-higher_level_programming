78964ffd98640108f6f94976e6a57b31d81f3600
luischaparoc <lfchaparro@outlook.com>
2019-10-22 22:09:25 +0000

    [Fix]: fixing style code with PEP8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,6 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ Initializes instances """
-        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
@@ -24,19 +23,21 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """ Getter size """
         return self.width
 
     @size.setter
     def size(self, value):
+        """ Setter size """
         self.width = value
         self.height = value
 
     def __str__(self):
         """ str special method """
-        str_rectangle = "[Rectangle] "
+        str_rectangle = "[Square] "
         str_id = "({}) ".format(self.id)
         str_xy = "{}/{} - ".format(self.x, self.y)
-        str_size = "{}".format(self.width)
+        str_size = "{}".format(self.size)
 
         return str_rectangle + str_id + str_xy + str_size
 
@@ -59,6 +60,7 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
+        """ Returns a dictionary with attributes """
         list_atr = ['id', 'size', 'x', 'y']
         dict_res = {}
 
