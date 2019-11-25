a7f1f76094f643dcb40e411c63bbcbfd99005569
luischaparoc <lfchaparro@outlook.com>
2019-11-25 17:32:10 +0000

    [Feature]: script that lists all records with a score >= 10 New file: 11-best_score.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records with a score >= 10
+-- Query to lists all records with a score >= 10 in the table second_table of a database
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
