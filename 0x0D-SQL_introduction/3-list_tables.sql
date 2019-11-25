e91368fbba3cab6dd4166d4925855d3c790487a5
luischaparoc <lfchaparro@outlook.com>
2019-11-25 16:16:10 +0000

    [Feature]: script that prints the full description of a table New file: 5-full_table.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
 -- Lists all the tables of a database
 -- Query to list all tables of a database
-SHOW TABLES
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the full description of the table first_table from the database
+-- Query to print full description of a table
+SHOW CREATE TABLE first_table;
