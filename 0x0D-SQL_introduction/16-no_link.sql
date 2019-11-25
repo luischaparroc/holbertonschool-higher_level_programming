1b5e967b292dd313d45d6a713ee25c19938967ec
luischaparoc <lfchaparro@outlook.com>
2019-11-25 20:38:25 +0000

    [Feature]: script that lists all records of a table New file: 16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records of a table
+-- Query to lists all records of the table second_table who have name value
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
