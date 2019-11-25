6b7375f5c883678e749b674d0cc945d92b8c28a5
luischaparoc <lfchaparro@outlook.com>
2019-11-25 15:54:28 +0000

    [Feature]: script that creates a table in the current database New file: 4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Creates a table in the current database
+-- Query to create a table called first_table in the current database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
