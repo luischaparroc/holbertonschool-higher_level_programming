1e43fc26c33791dae9051fd55a1e8105ea0d3b62
luischaparoc <lfchaparro@outlook.com>
2019-11-25 19:35:21 +0000

    [Feature]: script that computes the score average of all records New file: 14-average.sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Script that computes the score average of all records in a table
+-- Query to computes the score average of all records in the table second_table
+SELECT AVG(score) AS average FROM second_table;
