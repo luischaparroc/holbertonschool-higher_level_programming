4e702a7f17dedb3ae41496e942d48705a3625b2d
luischaparoc <lfchaparro@outlook.com>
2019-11-26 15:28:52 +0000

    [Feature]: script that lists all privileges of two users New file: 0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
