ac4f72cd206724db1f816618d8bc75f6a1e1b46e
luischaparoc <lfchaparro@outlook.com>
2019-11-26 16:03:42 +0000

    [Update]: don't fails if user exists Modified: 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Script that creates an user in MySQL server
 -- Query to create the user 'user_0d_1' in MySQL server
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
