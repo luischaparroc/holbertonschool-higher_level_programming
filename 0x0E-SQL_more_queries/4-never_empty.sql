d95a4eef3fea92082aeeae840e0475959ac46ca5
luischaparoc <lfchaparro@outlook.com>
2019-11-26 16:29:35 +0000

    [Feature]: adding a script that creates a table with a constraint New file: 4-never_empty.sql

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Script that creates a table
+-- Query to create the table 'id_not_null' in MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null (
+       id INT DEFAULT 1,
+       name VARCHAR(256));
