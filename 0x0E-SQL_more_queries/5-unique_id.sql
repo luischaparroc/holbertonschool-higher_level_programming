99f190cb7708d2b8693241171ff82f746c9c1726
luischaparoc <lfchaparro@outlook.com>
2019-11-26 16:47:03 +0000

    [Update]: SQL syntax Modified: 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,6 @@
--- Script that creates a table
--- Query to create the table 'unique_id' in MySQL server
-CREATE TABLE IF NOT EXISTS unique_id (
+-- Script that creates a database and a table
+CREATE DATABASE -- Query to create database hbtn_0d_usa
+IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states ( -- Query to create states table with 2 constraints
        id INT UNIQUE DEFAULT 1,
        name VARCHAR(256));
