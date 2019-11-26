2ce44fe7b7eb69e8e7d22f508f262638bb89d968
luischaparoc <lfchaparro@outlook.com>
2019-11-26 19:19:44 +0000

    [Feature]: script to list all cities of California in database New file: 8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Script that lists all the cities of California registered in the database
+SELECT * -- Query to list all the cities from California
+FROM cities
+WHERE state_id = ( -- Query to get the id of California
+      SELECT id
+      from states
+      WHERE name = "California" );
