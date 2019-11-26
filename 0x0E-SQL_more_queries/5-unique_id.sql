326bd96ee86de4fab9f5ff8305677e47d79ff450
luischaparoc <lfchaparro@outlook.com>
2019-11-26 16:48:45 +0000

    [Feature]: adding script Modified: 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
--- Script that creates a database and a table
-CREATE DATABASE -- Query to create database hbtn_0d_usa
-IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states ( -- Query to create states table with 2 constraints
+-- Script that creates a table
+-- Query to create the table 'unique_id' in MySQL server
+CREATE TABLE IF NOT EXISTS unique_id (
        id INT UNIQUE DEFAULT 1,
        name VARCHAR(256));
