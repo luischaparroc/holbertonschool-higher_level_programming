d4e88f344ddac091cf7b899f80aead142abde215
luischaparroc <lfchaparro@outlook.com>
2019-12-13 20:43:52 +0000

    [Feature]: adding a script that lists all states starting with N New file: 1-file_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
