68222b032f8b4a15080223ba797b157704c969d8
luischaparroc <lfchaparro@outlook.com>
2019-12-13 22:57:43 +0000

    [Update]: injection free in task 5 Modified: 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
     FROM cities JOIN states ON cities.state_id = states.id \
-    WHERE states.name = %s;", (sys.argv[4],))
+    WHERE states.name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
     print(", ".join([state[1] for state in states]))
