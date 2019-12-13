3855719b083a3a30ab2fbe03464d3eff98cdf750
luischaparroc <lfchaparro@outlook.com>
2019-12-13 21:33:02 +0000

    [Feature]: script that allows injection Modified: 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
     for state in states:
