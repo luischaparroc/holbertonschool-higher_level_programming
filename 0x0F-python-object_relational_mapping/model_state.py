ef5547315b1a8df7103d37363e26abf1f63fec68
luischaparroc <lfchaparro@outlook.com>
2019-12-14 17:35:50 +0000

    [Fix]: including metadata Modified: model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,10 +2,11 @@
 """
 Contains State class and Base, an instance of declarative_base()
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
     """
