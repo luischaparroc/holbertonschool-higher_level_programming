c1951bd63d5b0359c0c2776b90d705089f61d6ef
luischaparroc <lfchaparro@outlook.com>
2019-12-14 23:21:22 +0000

    [Fix]: removing cities class attribute Modified: model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,7 +4,6 @@ Contains State class and Base, an instance of declarative_base()
 """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
@@ -17,4 +16,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City')
