b1785d56a8c10d2236184ac3028190c3cd4af9e1
luischaparroc <lfchaparro@outlook.com>
2019-12-14 17:50:02 +0000

    [Fix]: pep8 on task 6 Modified: model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
     """
     Class with id and name attributes of each state
