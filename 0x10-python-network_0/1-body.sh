8a0a167fd42d48a2675159254088960b91de533f
luischaparroc <lfchaparro@outlook.com>
2020-01-06 14:25:45 -0500

    [Fix]: curls body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that shows the body if Status Code is 200
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1"
+# Script that shows the response body redirecting /route_1
+curl -Ls "$1"
