bbc1b9d1e202cde157fbe102e97a56578230e20d
luischaparroc <lfchaparro@outlook.com>
2020-01-13 19:16:30 +0000

    [Feature]: adding a script that uses GitHub API to display ids New file: 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes GitHub credentials
+and uses the GitHub API to display the user id
+"""
+from requests import get, auth
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
