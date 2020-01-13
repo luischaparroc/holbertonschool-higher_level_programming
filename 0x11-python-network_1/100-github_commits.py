6eabb7cce5d7327fa9376b12da63628ecaf8a707
luischaparroc <lfchaparro@outlook.com>
2020-01-13 20:09:10 +0000

    [Fix]: adding try/except

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,11 +8,14 @@ import sys
 
 
 if __name__ == "__main__":
-    repo = sys.argv[1]
-    owner = sys.argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    r = get(url)
-    json_o = r.json()
-    for i in range(0, 10):
-        print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
-                              .get('author').get('name')))
+    try:
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+        r = get(url)
+        json_o = r.json()
+        for i in range(0, 10):
+            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
+                                  .get('author').get('name')))
+    except:
+        pass
