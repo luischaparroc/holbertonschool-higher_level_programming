b392a9f7e45fc8c95c51faf8d4fa4bafc8b92e35
luischaparroc <lfchaparro@outlook.com>
2020-01-13 15:18:53 +0000

    [Update]: adding condition name == main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 import urllib.request
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
