9f4c6e994a247eeebf2e6b1533b76b190784ccb7
luischaparroc <lfchaparro@outlook.com>
2020-01-13 15:12:23 +0000

    [Update]: updating first line

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print('Body respoonse:')
+    print('Body response:')
     print('\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
     print('\t- utf8 content: {}'.format(html.decode("utf-8")))
