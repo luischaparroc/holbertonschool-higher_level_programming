00bb8ab92a4f1a225dd7a0df7ab81ff7f1ae5b3c
luischaparroc <lfchaparro@outlook.com>
2020-01-13 15:09:48 +0000

    [Feature]: adding a script that fetches the content of a web page New file: 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body respoonse:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode("utf-8")))
