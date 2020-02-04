9d20f93869cf2c8e594995b577d50a9774c3c7ef
luischaparroc <lfchaparro@outlook.com>
2020-02-04 11:25:13 -0500

    [Feature]: adding script that searches the second biggest integer New file: 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+let secondbiggest = 0;
+let biggest = 0;
+
+for (let i = 2; i < process.argv.length; i++) {
+  if (process.argv[i] > biggest) {
+    secondbiggest = biggest;
+    biggest = process.argv[i];
+  }
+  if ((process.argv[i] < biggest) && (process.argv[i] > secondbiggest)) {
+    secondbiggest = process.argv[i];
+  }
+}
+
+console.log(secondbiggest);
