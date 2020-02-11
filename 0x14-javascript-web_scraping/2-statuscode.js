f8cbd34cdb911d352392586a77c8f7c091c80146
luischaparroc <lfchaparro@outlook.com>
2020-02-11 12:45:23 -0500

    Fix 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 const request = require('request');
 request(process.argv[2], function (error, response) {
   if (error == null) {
-    console.log('code: ', response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
