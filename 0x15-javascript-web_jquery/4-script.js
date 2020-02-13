698aab845909472ba02220ce15acdf5384b466d7
luischaparroc <lfchaparro@outlook.com>
2020-02-13 14:13:38 -0500

    [Feature]: adding script that toggles the class to red or green New file: 4-script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$("DIV#toggle_header").click(function() {
+    if($("HEADER").hasClass("red")) {
+	$("HEADER").removeClass("red");
+	$("HEADER").addClass("green");
+    } else {
+	$("HEADER").removeClass("green");
+	$("HEADER").addClass("red");
+    }
+});
